3d7439d3c7f3b7747a7aca0d62405606c9ace8f3
serinkanasci <serinkan.asci7@gmail.com>
2020-10-02 11:28:08 +0200

    last changes

--- contract.sol
+++ contract.sol
@@ -4,18 +4,18 @@ contract HelloWorld
     address creator;
     string greeting;
 
-    function HelloWorld(string _greeting) public
+    constructor(string memory _greeting) public
     {
         creator = msg.sender;
         greeting = _greeting;
     }
 
-    function greet() constant returns (string)
+    function greet() public returns (string memory)
     {
         return greeting;
     }
 
-    function setGreeting(string _newgreeting)
+    function setGreeting(string memory _newgreeting) public
     {
         greeting = _newgreeting;
     }
